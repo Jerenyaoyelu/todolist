29ff88a1867401cc23ff950f96c128e9a17305d1
YAOYE LU <luyaoyejeren@gmail.com>
2019-10-12 20:32:38 +1100

    add model test

--- backend/tdl/tests.py
+++ backend/tdl/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.shortcuts import get_object_or_404
 
-# Create your tests here.
+from .models import Todos
+
+class TodoTest(TestCase):
+    def setUp(self):
+        Todos.objects.create(title="To buy a pair of shoes", progress="None")
+
+    # def test_add_content_to_title(self):
+    #     print("asas")
+    #     todo=get_object_or_404(Todos,id=1)
+    #     exp_obj_name = f'{todo.title}'
+    #     self.assertEquals(exp_obj_name,"To buy a pair of shoes")
+
+    def test_add_content_to_progress(self):
+        todo=get_object_or_404(Todos,id=1)
+        exp_obj_pgs=f'{todo.progress}'
+        self.assertEquals(exp_obj_pgs,"None")
