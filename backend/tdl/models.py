ea606e9fc136f348d98fd8eebb4c1b155b0fbe12
YAOYE LU <luyaoyejeren@gmail.com>
2019-10-12 19:10:54 +1100

    modify model fields

--- backend/tdl/models.py
+++ backend/tdl/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 class Todos(models.Model):
-    title = models.CharField(max_length=48)
-    description = models.TextField(max_length=1000)
+    title = models.CharField(max_length=100,blank=False)
+    progress = models.CharField(max_length=1000, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
+
 
     def __str__(self):
         # display todos brief
