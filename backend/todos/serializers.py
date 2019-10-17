7a3268dba7d58eef46d3085fd24b926ec46341c4
YAOYE LU <luyaoyejeren@gmail.com>
2019-10-17 13:02:20 +1100

    fix datetime format bugs

--- backend/todos/serializers.py
+++ backend/todos/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Todos
+import datetime
+from rest_framework.settings import api_settings
 
-class TodoSerializer(serializers.HyperlinkedModelSerializer):
+class TodoSerializer(serializers.ModelSerializer):
+    due=serializers.DateField(format=None,input_formats=None)
     class Meta:
         model=Todos
-        fields=['id','title','notes','created_at','completed',]
+        fields=['id','title','notes','due','completed',]
