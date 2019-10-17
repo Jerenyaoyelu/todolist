a4d5ba29750f3453f804607a11f5f5698d694d7d
YAOYE LU <luyaoyejeren@gmail.com>
2019-10-17 16:08:08 +1100

    completely fixed date format bugs, every actions works well

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+import datetime
 
 class Todos(models.Model):
     title = models.CharField(max_length=100,blank=False)
     notes = models.CharField(max_length=1000, blank=True)
     #have to format the datetime, otherwise when the date was entered as an input, error will occur because of wrong format
-    due = models.DateTimeField(auto_now_add=False)
+    due = models.DateField(default=datetime.date.today)
     completed = models.BooleanField(default=False)
 
 
--- backend/todos/serializers.py
+++ backend/todos/serializers.py
@@ -4,7 +4,7 @@ import datetime
 from rest_framework.settings import api_settings
 
 class TodoSerializer(serializers.ModelSerializer):
-    due=serializers.DateField(format=None,input_formats=None)
+    due=serializers.DateField(format="%Y-%m-%d",input_formats=['%Y-%m-%d',])
     class Meta:
         model=Todos
         fields=['id','title','notes','due','completed',]
